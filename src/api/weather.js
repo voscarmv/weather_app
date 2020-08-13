de6575cbb5a23675bc47f7dc20b8409206cb319e
Oscar Mier <voscarmv@gmail.com>
2020-08-13 11:15:10 -0500

    Added https to weather API

--- src/api/weather.js
+++ src/api/weather.js
@@ -4,7 +4,7 @@ async function weatherAPI(city, country) {
   try {
     console.log("hello from city api")
     const getWeather = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=b0ecf9b6e7ac316267e1a5acaf7c01d3`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=b0ecf9b6e7ac316267e1a5acaf7c01d3`,
       { mode: 'cors' }
     );
     const weatherJSON = await getWeather.json();
