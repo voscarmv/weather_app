721227c15555099ecc051f19e8c223f1665c02c7
Oscar Mier <voscarmv@gmail.com>
2020-08-14 15:39:13 -0500

    Build index.html for testing

--- src/pages/loading.js
+++
@@ -1,8 +0,0 @@
-import newElement from '../rendering/newelement';
-
-const Loading = (() => {
-  const loading = newElement('h1', null, 'Loading...');
-  return loading;
-})();
-
-export default Loading;
--- src/pages/notfound.js
+++
@@ -1,8 +0,0 @@
-import newElement from '../rendering/newelement';
-
-const Loading = (() => {
-  const loading = newElement('h1', null, 'City not found.');
-  return loading;
-})();
-
-export default Loading;
